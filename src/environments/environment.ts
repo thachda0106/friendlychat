316c4457c6d58d0a5e350b8edc45ab6feabf9729
Thach Da <thachbovjp@gmail.com>
2024-08-26 20:42:36 +0700

    add angularfire

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   firebase: {
-    apiKey: 'API_KEY',
-    authDomain: 'PROJECT_ID.firebaseapp.com',
-    databaseURL: 'https://PROJECT_ID.firebaseio.com',
-    projectId: 'PROJECT_ID',
-    storageBucket: 'PROJECT_ID.appspot.com',
-    messagingSenderId: 'SENDER_ID',
-    appId: 'APP_ID',
-    measurementId: 'G-MEASUREMENT_ID',
+    databaseURL: "https://PROJECT_ID.firebaseio.com",
+    measurementId: "G-MEASUREMENT_ID",
+    projectId: "friendly-chat-53103",
+    appId: "1:611091662748:web:17840b848e4471cfc12a59",
+    storageBucket: "friendly-chat-53103.appspot.com",
+    apiKey: "AIzaSyBZQn2KHOCL_fr-uMlbJJ6k7wSqx3xOg4s",
+    authDomain: "friendly-chat-53103.firebaseapp.com",
+    messagingSenderId: "611091662748",
   },
 };
